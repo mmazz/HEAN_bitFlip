907ee475c361c56401c342d6e3b7a3011b478820
mmazz <mazzantimatiass@gmail.com>
2024-09-19 12:47:59 -0300

    Installer

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-
+echo "Initializing"
 wget -N https://github.com/snucrypto/HEAAN/archive/refs/tags/1.0.tar.gz
 tar -xzf 1.0.tar.gz
 mv HEAAN-1.0/HEAAN/* ./
 rm -rf HEAAN-1.0
+echo ""
+echo "Patching"
 mv changes.diff src
 cd src
 patch -p1 < changes.diff
+echo "Patching compleate"
+echo ""
+echo "Building"
 cd ../lib
 make all -j16
+echo "Building campaign"
 cd ../campaigns
 make
